c18a451c8475668158745021ce8b81ac3d44cbcc
gorgony <n.j.vannispen@gmail.com>
2017-11-06 22:12:49 +0100

    Fixed issue #2

--- src/main/java/teammates/ui/controller/AdminAccountDeleteAction.java
+++ src/main/java/teammates/ui/controller/AdminAccountDeleteAction.java
@@ -17,43 +17,54 @@ public class AdminAccountDeleteAction extends Action {
         String courseId = getRequestParamValue(Const.ParamsNames.COURSE_ID);
         String account = getRequestParamValue("account");
 
-        //TODO: We should extract these into separate actions e.g., AdminInstructorDowngradeAction
         if (courseId == null && account == null) {
-            //delete instructor status
-            logic.downgradeInstructorToStudentCascade(instructorId);
-            statusToUser.add(new StatusMessage(Const.StatusMessages.INSTRUCTOR_STATUS_DELETED, StatusMessageColor.SUCCESS));
-            statusToAdmin = "Instructor Status for <span class=\"bold\">" + instructorId + "</span> has been deleted.";
-            return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_MANAGEMENT_PAGE);
+            return deleteInstructorStatus(instructorId);
         }
 
         if (courseId == null && account != null) {
-            //delete entire account
-            logic.deleteAccount(instructorId);
-            statusToUser.add(new StatusMessage(Const.StatusMessages.INSTRUCTOR_ACCOUNT_DELETED, StatusMessageColor.SUCCESS));
-            statusToAdmin = "Instructor Account for <span class=\"bold\">" + instructorId + "</span> has been deleted.";
-            return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_MANAGEMENT_PAGE);
+            return deleteInstructorAccount(instructorId);
         }
 
         String studentId = getRequestParamValue(Const.ParamsNames.STUDENT_ID);
         if (courseId != null && studentId != null) {
-            //remove student from course
-            StudentAttributes student = logic.getStudentForGoogleId(courseId, studentId);
-            logic.deleteStudent(courseId, student.email);
-            statusToUser.add(new StatusMessage(Const.StatusMessages.STUDENT_DELETED,
-                                               StatusMessageColor.SUCCESS));
-            statusToAdmin = "Instructor <span class=\"bold\">" + instructorId
-                            + "</span>'s student status in Course"
-                            + "<span class=\"bold\">[" + courseId + "]</span> has been deleted";
-            return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_DETAILS_PAGE + "?instructorid=" + studentId);
+            return removeStudentFromCourse(instructorId, courseId, studentId);
         }
 
-        //remove instructor from course
+        return removeInstructorFromCourse(instructorId, courseId);
+    }
+
+    private ActionResult removeInstructorFromCourse(String instructorId, String courseId) {
         InstructorAttributes instructor = logic.getInstructorForGoogleId(courseId, instructorId);
         logic.deleteInstructor(courseId, instructor.email);
         statusToUser.add(new StatusMessage(Const.StatusMessages.INSTRUCTOR_REMOVED_FROM_COURSE, StatusMessageColor.SUCCESS));
         statusToAdmin = "Instructor <span class=\"bold\">" + instructorId
-                      + "</span> has been deleted from Course<span class=\"bold\">[" + courseId + "]</span>";
+                + "</span> has been deleted from Course<span class=\"bold\">[" + courseId + "]</span>";
         return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_DETAILS_PAGE + "?instructorid=" + instructorId);
+    }
+
+    private ActionResult removeStudentFromCourse(String instructorId, String courseId, String studentId) {
+        StudentAttributes student = logic.getStudentForGoogleId(courseId, studentId);
+        logic.deleteStudent(courseId, student.email);
+        statusToUser.add(new StatusMessage(Const.StatusMessages.STUDENT_DELETED,
+                StatusMessageColor.SUCCESS));
+        statusToAdmin = "Instructor <span class=\"bold\">" + instructorId
+                + "</span>'s student status in Course"
+                + "<span class=\"bold\">[" + courseId + "]</span> has been deleted";
+        return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_DETAILS_PAGE + "?instructorid=" + studentId);
+    }
+
+    private ActionResult deleteInstructorAccount(String instructorId) {
+        logic.deleteAccount(instructorId);
+        statusToUser.add(new StatusMessage(Const.StatusMessages.INSTRUCTOR_ACCOUNT_DELETED, StatusMessageColor.SUCCESS));
+        statusToAdmin = "Instructor Account for <span class=\"bold\">" + instructorId + "</span> has been deleted.";
+        return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_MANAGEMENT_PAGE);
+    }
+
+    private ActionResult deleteInstructorStatus(String instructorId) {
+        logic.downgradeInstructorToStudentCascade(instructorId);
+        statusToUser.add(new StatusMessage(Const.StatusMessages.INSTRUCTOR_STATUS_DELETED, StatusMessageColor.SUCCESS));
+        statusToAdmin = "Instructor Status for <span class=\"bold\">" + instructorId + "</span> has been deleted.";
+        return createRedirectResult(Const.ActionURIs.ADMIN_ACCOUNT_MANAGEMENT_PAGE);
     }
 
 }
